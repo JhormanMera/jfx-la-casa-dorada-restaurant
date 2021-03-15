2699cfdca267377fd54530b8f7e739223005696f
JhormanMera <jhorman.mera1@correo.icesi.edu.co>
2021-03-14 20:42:48 -0500

    Restaurant v3
    New classes implemented and product class finished

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Product {
+import java.util.ArrayList;
+
+public class Product{
 	/*
 	 * El sistema debe estar en la capacidad de gestionar (crear, actualizar, eliminar y deshabilitar) todos los productos que ofrece. 
 	 * Todo producto tiene un nombre, un tipo (plato principal, adicional o bebida -pero estos pueden variar-), un conjunto de ingredientes,
@@ -8,12 +10,17 @@ public class Product {
 	 */
 	private String name;
 	private ProductType type;
-	private Ingredients[] ingredients;
+	private ArrayList<Ingredients> ingredients;
+	private boolean available;
+	private double price;
+	private String productSize;
 	
-	public Product (String name, ProductType type, Ingredients[] ingredients) {
-		this.setName(name);
-		this.setType(type);
-		this.setIngredients(ingredients);
+	public Product (String name, ProductType type, Ingredients ingredients, double price) {
+		this.name=name;
+		this.type=type;
+		this.ingredients= new ArrayList<>();
+		this.available=true;
+		this.price=price;
 	}
 	public String getName() {
 		return name;
@@ -27,10 +34,37 @@ public class Product {
 	public void setType(ProductType type) {
 		this.type = type;
 	}
-	public Ingredients[] getIngredients() {
+	public ArrayList<Ingredients> getIngredients() {
 		return ingredients;
 	}
-	public void setIngredients(Ingredients[] ingredients) {
-		this.ingredients = ingredients;
+	public void setIngredients(Ingredients ingredients) {
+		this.ingredients.add(ingredients);
 	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getProductSize() {
+		return productSize;
+	}
+	public void setProductSize(String productSize) {
+		if (productSize.equalsIgnoreCase("")) {
+			this.productSize = ProductSize.valueOf("").toString();
+		} else if (productSize.equalsIgnoreCase("")) {
+			this.productSize = ProductSize.valueOf("").toString();
+		} else if (productSize.equalsIgnoreCase("")) {
+			this.productSize = ProductSize.valueOf("").toString();
+		} else if (productSize.equalsIgnoreCase("")) {
+			this.productSize = ProductSize.valueOf("").toString();
+		}
+	}
+
 }
--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -5,5 +5,4 @@ public class Restaurant {
 	public Restaurant() {
 		
 	}
-	
 }
